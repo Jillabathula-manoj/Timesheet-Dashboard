63735dcbe72168d94fcaf6e99127072955c9f2b2
Manoj jillabathula <manoj.jillabathula@vignosoft.com>
2024-04-18 09:50:20 +0530

    1st commit

---
+++ src/pages/DashboardHttpsbitlyavi.js
@@ -0,0 +1,234 @@
+import "./DashboardHttpsbitlyavi.css";
+
+const DashboardHttpsbitlyavi = () => {
+  return (
+    <div className="dashboard-httpsbitlyavi">
+      <div className="product">
+        <img className="product-child" alt="" src="/rectangle-30.svg" />
+        <div className="showing-data-1">
+          Showing data 1 to 8 of 256K entries
+        </div>
+        <div className="all-customers">All Timesheets</div>
+       
+        <div className="group-parent">
+          <div className="customer-name-parent">
+            <div className="customer-name">Name</div>
+            <div className="company">Department</div>
+            <div className="phone-number">Company</div>
+            <div className="email">Week Ending</div>
+            <div className="country">Total Hours</div>
+            
+            <div className="status">Status</div>
+          </div>
+          <div className="jane-cooper-parent">
+            <div className="jane-cooper">Jane Cooper</div>
+            <div className="microsoft">IT</div>
+            <div className="div">Rapid Tech Serv</div>
+            <div className="janemicrosoftcom">10-Dec-2023</div>
+            <div className="united-states">40 </div>
+            <div className="active-wrapper">
+              <div className="active">Pending</div>
+            </div>
+          </div>
+          <div className="floyd-miles-parent">
+          <div className="jane-cooper">Jane Cooper</div>
+            <div className="microsoft">IT</div>
+            <div className="div">Rapid Tech Serv</div>
+            <div className="janemicrosoftcom">10-Dec-2023</div>
+            <div className="united-states">40 </div>
+            <div className="active-wrapper">
+              <div className="active">Pending</div>
+            </div>
+          </div>
+          <div className="ronald-richards-parent">
+          <div className="jane-cooper">Jane Cooper</div>
+            <div className="microsoft">IT</div>
+            <div className="div">Rapid Tech Serv</div>
+            <div className="janemicrosoftcom">10-Dec-2023</div>
+            <div className="united-states">40 </div>
+            <div className="active-wrapper">
+              <div className="active">Pending</div>
+            </div>
+          </div>
+          <div className="marvin-mckinney-parent">
+          <div className="jane-cooper">Jane Cooper</div>
+            <div className="microsoft">IT</div>
+            <div className="div">Rapid Tech Serv</div>
+            <div className="janemicrosoftcom">10-Dec-2023</div>
+            <div className="united-states">40 </div>
+            <div className="active-wrapper">
+              <div className="active">Pending</div>
+            </div>
+          </div>
+          <div className="jerome-bell-parent">
+          <div className="jane-cooper">Jane Cooper</div>
+            <div className="microsoft">IT</div>
+            <div className="div">Rapid Tech Serv</div>
+            <div className="janemicrosoftcom">10-Dec-2023</div>
+            <div className="united-states">40 </div>
+            <div className="active-wrapper">
+              <div className="active">Pending</div>
+            </div>
+          </div>
+        
+          
+         
+          <div className="group-child" />
+          <div className="group-item" />
+          <div className="group-inner" />
+          
+        </div>
+        
+        <div className="frame-parent">
+          <div className="wrapper">
+            <div className="div8">1</div>
+          </div>
+          <div className="container">
+            <div className="div8">2</div>
+          </div>
+          <div className="frame">
+            <div className="div8">4</div>
+          </div>
+          <div className="wrapper1">
+            <div className="div8">40</div>
+          </div>
+          <div className="wrapper2">
+            <div className="div8">{`>`}</div>
+          </div>
+          <div className="wrapper3">
+            <div className="div8">{`<`}</div>
+          </div>
+          <div className="div14">...</div>
+          <div className="wrapper4">
+            <div className="div8">3</div>
+          </div>
+        </div>
+      </div>
+      <div className="search1">
+        <div className="search-child" />
+        <div className="search">Search</div>
+        <img className="search-1-icon" alt="" src="/search-1.svg" />
+      </div>
+      <div className="side-menu">
+        <div className="side-menu-child" />
+        <div className="side-menu-item" />
+        <div className="list-menu">
+          <img
+            className="icon-24-outline-key-squa"
+            alt=""
+            src="/icon--24--outline--keysquare.svg"
+          />
+          <div className="dashboard">Dashboard</div>
+        </div>
+        <div className="list-menu1">
+          <img
+            className="icon-24-outline-key-squa"
+            alt=""
+            src="/3dsquare-1.svg" 
+          />
+          <div className="customers">Add Candidate</div>
+          <img
+            className="chevron-right-2-icon"
+            alt=""
+            src="/chevronright-2.svg"
+          />
+        </div>
+        <div className="list-menu2">
+          <img
+            className="icon-24-outline-key-squa"
+            alt=""
+            src="/usersquare-1.svg"
+          />
+          <div className="customers">Assignment Dashboard</div>
+          <img
+            className="chevron-right-2-icon"
+            alt=""
+            src="/chevronright-21.svg"
+          />
+        </div>
+        <div className="list-menu3">
+          <img
+            className="icon-24-outline-key-squa"
+            alt=""
+            src="/walletmoney-2.svg"
+          />
+          <div className="customers">My Timesheet</div>
+          <img
+            className="chevron-right-2-icon"
+            alt=""
+            src="/chevronright-2.svg"
+          />
+        </div>
+        <div className="list-menu4">
+          <img
+            className="icon-24-outline-key-squa"
+            alt=""
+            src="/discountshape-1.svg"
+          />
+          <div className="customers">Approve Timesheet</div>
+          <img
+            className="chevron-right-2-icon"
+            alt=""
+            src="/chevronright-2.svg"
+          />
+        </div>
+        <div className="list-menu5">
+          <img
+            className="icon-24-outline-key-squa"
+            alt=""
+            src="/messagequestion-1.svg"
+          />
+          <div className="customers">Login to HRMS</div>
+          <img
+            className="chevron-right-2-icon"
+            alt=""
+            src="/chevronright-2.svg"
+          />
+        </div>
+        <img className="side-menu-inner" alt="" src="logout-exit.png" />
+        <div className="evano-parent">
+          <div className="evano">Sign Out</div>
+          
+        </div>
+       
+        <div className="dashboard-parent">
+          <div className="dashboard1">Rapid Technologies</div>
+          <img className="setting-1-icon" alt="" src="/setting-1.svg" />
+        </div>
+        <div className="v01">v.01</div>
+      </div>
+
+      <div className="hello-evano">Approved Timesheet</div>
+<div className="earning">
+  <img className="earning-child" alt="" src="/rectangle-15.svg" />
+  <div className="group-container">
+    <div className="total-customers-parent">
+      <div className="total-customers">Total </div>
+      <div className="div16">64</div>
+      <img className="arrow-up-1-icon2" alt="" src="/hashtag.jpg" />
+    </div>
+    <div className="members-parent">
+      <div className="total-customers">Pending</div>
+      <div className="div16">64</div>
+   
+    </div>
+    <div className="active-now-parent">
+      <div className="total-customers">Approved</div>
+      <div className="div16">64</div>
+      <img className="arrow-up-1-icon1" alt="" src="/pending.svg" />
+      
+    </div>
+    <div className="active-now-parent-to">
+    <img className="arrow-up-1-icon1" alt="" src="/Approved.svg" />
+      </div>
+  </div>
+</div>
+
+
+</div>
+
+  );
+
+};
+
+export default DashboardHttpsbitlyavi;
